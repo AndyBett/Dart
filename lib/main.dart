537d598d593f53295a19c6118e8d483dd5d8a415
Andy . <andybett99@gmail.com>
2024-10-13 19:53:05 +0300

    dart practice1

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+void main() {
+
+}
+
+class Bike{
+
+  //properties
+
+  String color = "red";
+  num CC = 150;
+
+  //Methods - functions used in classes
+
+  void rideBike()
+  {
+    //starting an engine
+    //Moving Forward
+    print("$color Bike of $CC CC is moving forward!");
+  }
+
+
+}
